ed4201eaebeca15ab6a43cea3466d844fab29958
jonghoon <jonghoon.1738@gmail.com>
2024-11-19 22:55:22 +0900

    Init

---
+++ PoupProvider/MyPopupViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace PoupProvider;
+
+public partial class MyPopupViewModel : ObservableObject
+{
+    public TaskCompletionSource<List<string>> Task { get; set; }
+
+    [RelayCommand]
+    public void ResultSend()
+    {
+        Task.SetResult(new List<string> { "Result1", "Result2", "Result3" });
+    }
+}
---
+++ PoupProvider/ViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace PoupProvider;
+
+public partial class ViewModel : ObservableObject
+{
+    public ViewModel()
+    {
+        WeakReferenceMessenger.Default.Register<PopupMessage>(Global.obj, (obj, message) =>
+        {
+            PopupContent = message.Content;
+        });
+
+    }
+
+    [ObservableProperty]
+    private object _popupContent;
+
+
+    [RelayCommand]
+    public async Task Test()
+    {
+        var value = await new PopupProvider().GetPopupItems();
+        Console.WriteLine(value);
+    }
+}
+
+//create global singleton class
+public static class Global
+{
+    public static void EmptyMethod() { }
+
+    public static object obj = new object();
+}
+
+public class PopupMessage
+{
+    public object Content { get; set; }
+}
+
+public class PopupProvider
+{
+    public async Task<List<string>> GetPopupItems()
+    {
+        var popup = new MyPopup();
+        var vm  = new MyPopupViewModel();
+        popup.DataContext = vm;
+        vm.Task = new TaskCompletionSource<List<string>>();
+        WeakReferenceMessenger.Default.Send(new PopupMessage { Content = popup });
+        var data = await vm.Task.Task;
+        return data;
+    }
+}
